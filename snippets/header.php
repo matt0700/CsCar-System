65d75e81d6794d504e90a6fe6aa70919da550e84
Duraeu <152680183+Duraeu@users.noreply.github.com>
2024-06-16 22:00:21 +0800

    Update header.php

--- snippets/header.php
+++ snippets/header.php
@@ -1,27 +1,129 @@
-<body>
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body{
+                font-family: Arial;
+            }
 
-</body>
+            .header{
+                display:flex;
+                flex-direction: row;
+                justify-content: space-between;
+                height:200px;
+                margin-left:280px;
+                padding-top: 20px;
+            }
+
+            .left-section{
+                display:flex;
+                flex-direction: column;
+            }
+
+            .Cscar-sidebar{
+                color:White;   
+                display: flex;
+                justify-content: center;
+            }
+
+            
+            .sidebar{
+                position:fixed;
+                bottom:0px;
+                left:0px;
+                top: 0px;
+                background-color: black ;
+                width:250px;
+              
+            }
+
+            .Cscar-sidebar{
+                font-size:20px;
+                margin-top:20px;
+                
+            }
+
+            .options{
+                display: flex;
+                flex-direction: column;
+                align-items:center;
+                height:500px;
+                color:white;
+                background-color: black;
+                margin-top:25px;w
+            }
+
+            .btn-sidebar{
+                margin-top:30px;
+            }
+
+            .btn-design-sidebar{
+                background-color: black;
+                color:white;
+                border:none;
+                font-size:16px;
+            }
+
+            .help-support-btn{
+                display:flex;
+                justify-content: center;
+                max-height:50px;
+                margin-top:20px;
+                
+
+            }
+
+
+
+        </style>
+    </head>
+
+    <body style="height: 1000px;">
+
+        <div class="header">
+
+            <div class="left-section">
+                <div class="welcome-text">Welcome back, Matthew</div>
+                <div class="general-function">Track, manage and forecast your clients, schedules, and maintenance</div>
+            </div>
+            <div class="right-section">
+                <div><button>Bell</button></div>
+                <div>
+                <div>Escarlet Conde</div>
+                <div>Admin</div>
+                </div>
+                <div>menu</div>
+            </div>
+
+
+
+        </div>
+       
+        <div class="sidebar">
+
+            <div class="Cscar-sidebar"><p>CSCar</p></div>
+
+            <div class="options">
+                <div class="btn-sidebar"><button class="btn-design-sidebar">Dashboard</button></div>
+
+                <div class="btn-sidebar"><button class="btn-design-sidebar">Fleet Management</button></div>
+
+                <div class="btn-sidebar"><button class="btn-design-sidebar">Schedules</button></div>
+                <div class="btn-sidebar"><button class="btn-design-sidebar">Drivers</button></div>
+                <div class="btn-sidebar"><button class="btn-design-sidebar">Users</button></div>
+                <div class="btn-sidebar"><button class="btn-design-sidebar">Settings</button></div>
+            </div>
+
+            <div class="help-support-btn"><button class="btn-design-sidebar">Help and Suppport</button></div>
+
+           
+
+        </div>
+
+
+
+    </body>
+</html>
+
+
+
