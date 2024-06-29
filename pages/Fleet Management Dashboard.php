385f20d97dc3794a1e5cca34a5265b08e3868915
matt0700 <89130475+matt0700@users.noreply.github.com>
2024-06-29 11:45:41 +0800

    update
    title update lang

--- pages/Fleet Management Dashboard.php
+++ pages/Fleet Management Dashboard.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main Dashboard</title>
+    <title>Fleet Management Dashboard</title>
     <?php include '../snippets/header.php'; ?>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
@@ -20,26 +20,7 @@
   <button class="w3-button w3-greyw3-xlarge w3-hide-large " onclick="w3_open()">&#9776;</button>
   
   <div class="w3-container flex  " style="color: white;">
-
-    <div class="flex-col">
-
-    <div>
-      <h1>Welcome, _____ </h1>
-    </div>
-    Track, manage and forecast your clients, schedules, and maintenance
-    <div>
-
-    </div>
-    </div>
-  
-    <div class="flex  w3-display-topright w3-margin-right">
-                <div><button>Bell</button></div>
-                <div>
-                <div>Escarlet Conde</div> <!--ADMIN NAME-->
-                <div>Admin</div> <!--POSITION-->
-                </div>
-                <div>menu</div> <!--LOG OUT-->
-            </div>
+    Fleet Management
   </div>
 </div>
 
@@ -53,11 +34,7 @@
       <div><!--MAPBOX--></div>
     </div>
 
-    <div class="bg-gray-300 rounded-sm min-h-[180px] min-w-[140px] border-4 border-black">Statictics</div>
-
-    <div class="bg-gray-300 rounded-sm min-h-[180px] min-w-[180px] border-4 border-black">Schedules</div>
-
-    <div class="bg-gray-300 rounded-sm min-h-[180px] min-w-[140px] border-4 border-black">Vehichles in Used</div>
+    
   </div>
 </div>
   
