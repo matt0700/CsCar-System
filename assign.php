9b67296435889566a14e7ebf5c4a334fcdfd28b0
ryancunanan34 <ryancunanan348@gmail.com>
2024-07-04 21:15:15 +0800

    Create assign.php

---
+++ assign.php
@@ -0,0 +1,68 @@
+<?php
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'dbname';
+
+//Connection
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Check connection
+
+if ($conn->connect_error){
+    die("Connection failed: " . $conn->connect_error);
+
+}
+
+//Fetched unassigned Trips
+$trips_sql = 'SELECT * FROM trips WHERE ruv_table NOT IN (SELECT ruvNO FROM trips)';
+$trips_result = $conn->query($trips_sql);
+
+if($trips_result->num_rows > 0){
+    while ($trip = $trips_result->fetch_assoc()){
+        //Find Available driver
+        $driver_sql = "SELECT * FROM driver_info WHERE status = 'Available' LIMIT 1";
+        $driver_result = $conn->query($driver_sql);
+        $driver = $driver_result->fetch_assoc();
+
+        //Gind available vehicle that can accomodate number of passengers
+
+        $vehicle_sql = "SELECT * FROM vehicle_data WHERE status= 'Available' AND seater >= " . $trip['no_passengers'] . " LIMIT 1";
+        $vehicle_result = $conn->query($vehicle_sql);
+        $vehicle = $vehicle_result->fetch_assoc();
+
+        if ($driver && $vehicle){
+            //Insert new trip
+            $assign_sql = "INSERT INTO trips (ruvNO, driver_id, plate_no, trip_date) VALUES (" . $trip['ruvNO'] . " , 
+            ".$driver['driver_id'] . " , " . $vehicle['plate_no']." , CURDATE())";
+            $conn->query($assign_sql);
+
+            //Update driver status
+            $update_driver_sql = "UPDATE driver_info SET status = 'Unavailable' WHERE driver_id = " . $driver['driver_id'];
+            $conn->query($update_driver_sql);
+
+            //Update vehicle status
+            $update_vehicle_sql = "UPDATE vehicle_data SET status = 'Unavailable' WHERE plate_no = " . $vehicle['plate_no'];
+            $conn->query($update_vehicle_sql);
+        
+        }else{
+            echo "No available driver or vehicle for trip " . $trip['ruvNO'] . " with " . $trip['no_passengers'] . " passengers.\n";
+        }
+    }
+} else{
+        echo "No assigned trips found.\n";
+}
+
+
+$conn->close();
+
+?>
+
+
+
+
+
+
+?>
