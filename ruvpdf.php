b4046a7d5d37ec92fae9d30d8cdc338b23f5670a
ryancunanan34 <ryancunanan348@gmail.com>
2024-07-14 21:15:18 +0800

    ruv pdf

---
+++ ruvpdf.php
@@ -0,0 +1,25 @@
+<?php
+$con = new PDO("mysql:host=localhost;dbname=cscar_database","root","");
+$query = "SELECT ruvNO FROM ruv_table";
+$result = $con->prepare($query);
+$result->execute();
+if($result->rowCount()){
+    while($ruvNO = $result->fetch())
+    {
+        ?>
+        <tr>
+            <td>
+                <a href="append.php?ruvNO=<?php echo $ruvNO['ruvNO'];
+                ?>">view online</a>
+            </td>
+            <td>
+                <a href="append.php?ruvNO=<?php echo $ruvNO['ruvNO'];
+                ?>">Download Now</a>
+            </td>
+        </tr>
+        <?php
+    }
+}else{
+    echo "<br><br>Data Not Found";
+}
+?>
